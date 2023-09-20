8b0933776d68c340654d72392f438bc20398ebc4
JonathanWalston <145593517+JonathanWalston@users.noreply.github.com>
2023-09-20 10:32:53 -0700

    Initial Upload of main.py
    The main.py script serves as the entry point for the entire analysis workflow, automating the execution of three major components of the python code files.
    
    Data Collection: Calls data_collection.collect_data() to collect the required financial data.
    CAR Calculation: Invokes car_calculation.calculate_all_cars() to compute Cumulative Abnormal Returns (CAR) based on the collected data.
    Statistical Analysis: Executes car_stats.calculate_stats() to perform a variety of statistical tests on the calculated CAR data, apply multiple testing correction, and export results to a CSV file.
    By running main.py, users can efficiently execute these tasks in a streamlined manner, making the workflow accessible for all users.

---
+++ main.py
@@ -0,0 +1,12 @@
+import data_collection
+import car_calculation
+import car_stats
+
+# Collect data
+data_collection.collect_data()
+
+# Calculate CAR
+car_calculation.calculate_all_cars()
+
+# Calculate Statistics
+car_stats.calculate_stats()
