0bd8f5da706cc0e53e6892841a4c5a5be4a9886e
JonathanWalston <145593517+JonathanWalston@users.noreply.github.com>
2023-09-20 11:50:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,37 @@ import data_collection
 import car_calculation
 import car_stats
 
-# Collect data
-data_collection.collect_data()
 
-# Calculate CAR
-car_calculation.calculate_all_cars()
+def main():
+    # Collect data
+    print("Starting data collection...")
+    try:
+        data_collection.collect_data()
+        print("Data collection completed successfully.")
+    except Exception as e:
+        print(f"Data collection failed with error: {e}")
+        return
 
-# Calculate Statistics
-car_stats.calculate_stats()
+    # Calculate CAR
+    print("Starting CAR calculation...")
+    try:
+        car_calculation.calculate_all_cars()
+        print("CAR calculation completed successfully.")
+    except Exception as e:
+        print(f"CAR calculation failed with error: {e}")
+        return
+
+    # Calculate Statistics
+    print("Starting statistics calculation...")
+    try:
+        car_stats.calculate_stats()
+        print("Statistics calculation completed successfully.")
+    except Exception as e:
+        print(f"Statistics calculation failed with error: {e}")
+        return
+
+    print("All tasks completed successfully.")
+
+
+if __name__ == '__main__':
+    main()
